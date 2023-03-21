c12888285cb99f420a02bb670ef665b5fa9695a0
Krishna Abbina <kabbina@pcgus.com>
2023-03-21 13:21:24 +0530

    added postman.js for creating the postman code snippet

---
+++ postman.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');   
+const codegen = require('postman-code-generators');
+const sdk = require('postman-collection');
+const request = new sdk.Request('http://localhost:3001/api/request');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = 3001;
+
+let options = {
+    indentCount: 3,
+    indentType: 'Space',
+    trimRequestBody: true,
+    followRedirect: true
+};
+
+app.get('/getLanguages', (req, res) => {
+  res.send(codegen.getLanguageList())
+})
+
+app.get('/getCode/:lv', (req, res) => {
+    console.log(req.params);
+    let[language, variant] = req.params.lv.split('_');
+    codegen.convert(language, variant, request, options, function(error, snippet) {
+        if (error) {
+            res.send(error);
+        }
+        res.send(snippet);
+    });  
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
