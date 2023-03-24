dbc529c1ea5edb0cf77a437a570f24580973fc95
Krishna Abbina <kabbina@pcgus.com>
2023-03-24 11:54:18 +0530

    logic for looping through languages

--- src/routes/services.js
+++ src/routes/services.js
@@ -3,13 +3,14 @@ import Typography from '@mui/joy/Typography';
 import Box from '@mui/joy/Box';
 
 export default function Services() {
+  const [languages, setLanguages] = React.useState([]);
   React.useEffect(()=> {
     fetch('http://localhost:3001/getLanguages', {method: "GET",
       headers: {
       "Content-Type": "text/json;charset=UTF-8"
       },
     })
-    .then(response => response.json().then((data) => console.log(data)))
+    .then(response => response.json().then((data) => setLanguages(data)))
     .catch(error => {
       console.log(error);
     });
@@ -17,7 +18,16 @@ export default function Services() {
 
   return (
     <Box lineHeight="lg">
-      <Typography level="h1" fontSize="1.5rem" fontWeight="lg"> Services</Typography>
+      <Typography level="h1" fontSize="1.5rem" fontWeight="lg">Services</Typography>
+      <ul>
+        {
+          languages.map((l) =>
+            l.variants.map((v) =>
+              <li key={l.label + '_' + v.key}>{l.label} - {v.key}</li>
+            )
+          )
+        }
+      </ul>
     </Box>
   );
 }
